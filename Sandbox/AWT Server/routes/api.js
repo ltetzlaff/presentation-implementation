8e6c711b26b36d221a0957163c9818e1afd3d34c
MasonVX <masonvx@gmail.com>
2017-01-29 13:49:01 +0100

    Implemented longpolling TODO: put monitorCollection and Longpolling together.

--- Sandbox/AWT Server/routes/api.js
+++ Sandbox/AWT Server/routes/api.js
@@ -3,6 +3,15 @@ var router = express.Router();
 var monitorCollection = require('../monitorCollection');
 var log = require('../log.js');
 
+/**
+ * 
+ * Eventmanagement
+ * 
+ */
+var EventEmitter = require('events').EventEmitter;
+var messageBus = new EventEmitter();
+messageBus.setMaxListeners(100);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.json({message:'hooray!'});
@@ -48,17 +57,36 @@ router.get('/connection/:id', function(req, res, next) {
 /**  Send data to monitor
 * @param :id session id
 */
-router.post('/send/:id', (req, res) => {
-  throw new Error("Not yet implemented");
+router.post('/message/:id', (req, res) => {
+  res.status(200).end();
+	messageBus.emit('message' + req.params.id, req.body);  
 })
 
 /**  Receive data from monitor (longpolling)
 * @param :id session id
 */
-router.post('/receive/:id', (req, res) => {
-  throw new Error("Not yet implemented");
-})
-
+router.get('/message/:id', (req, res) => {
+  if (req.query.state == -1) {
+    res.status(200).end("Timeout");
+    return;
+  }
+  const listenerFun = function (data) {
+    res.json(data);
+  };
+  const addMessageListener = function (res) {
+    messageBus.once('message' + req.params.id, listenerFun)
+  };
+  req.on("close", function () {
+    //console.log(listenerFun());
+    messageBus.removeListener('message' + req.params.id, listenerFun);
+    console.log("Connection closed for: " + req.params.id + ' Events:' + messageBus.listenerCount());
+  });
+  addMessageListener(res);
+  setTimeout(function () {
+    messageBus.removeListener('message' + req.params.id, listenerFun);
+    res.status(200).end("Timeout");
+  }, 20000);
+});
 
 
 module.exports = router;
